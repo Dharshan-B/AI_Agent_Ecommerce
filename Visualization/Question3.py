2f16540f36792fbe841190d3724ea909ce161aa9
Dharshan B <dharshanb0611@gmail.com>
2025-07-23 10:04:46 +0530

    Screenshots

--- Visualization/Question3.py
+++ Visualization/Question3.py
@@ -2,18 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-ad_sales_df = pd.read_csv("Product-Level-Ad-Sales-and-Metrics.csv")
-total_sales_df = pd.read_csv("Product-Level-Total-Sales-and-Metrics.csv")
 
-sns.set(style="whitegrid")
+df_eligibility = pd.read_csv('data/Product-Level-Eligibility-Table.csv')
 
+
+df_eligibility['eligibility_date'] = pd.to_datetime(df_eligibility['eligibility_datetime_utc']).dt.date
+df_eligibility['eligibility_date'] = pd.to_datetime(df_eligibility['eligibility_date']) # Convert back to datetime for plotting
+
+
+eligible_products = df_eligibility[df_eligibility['eligibility'] == True]
+
+
+eligible_products_count = eligible_products.groupby('eligibility_date').size().reset_index(name='num_eligible_products')
 
 plt.figure(figsize=(12, 6))
-top_cpc_df = ad_sales_df.sort_values(by="CPC", ascending=False).head(10)
-sns.barplot(x="Product Name", y="CPC", data=top_cpc_df)
-plt.title("Top 10 Products by CPC")
-plt.xlabel("Product")
-plt.ylabel("Cost Per Click (CPC)")
-plt.xticks(rotation=45, ha='right')
+sns.lineplot(x='eligibility_date', y='num_eligible_products', data=eligible_products_count)
+plt.title('Number of Eligible Products Over Time')
+plt.xlabel('Date')
+plt.ylabel('Number of Eligible Products')
+plt.grid(True)
 plt.tight_layout()
-plt.show()
+plt.savefig('num_eligible_products_over_time.png')
+plt.show() 
